1c80bc26d0abe065d7165a3b94d554d2268dc1c9
Steve Wyman <cloudventiresonline@gmail.com>
2026-03-14 09:56:48 +0000

    feat: customize with Bournemouth data and generate dynamic pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://manchesterblockeddrain.co.uk/',
+  site: 'https://PlumbersBournemouth247.co.uk/',
   integrations: [
     sitemap(),
     tailwind(),
